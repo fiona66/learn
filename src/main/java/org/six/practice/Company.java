0663bb43e51a34565101a24d4b85de66658cd0b4
fiona66 <lulu_fiona@163.com>
2017-03-20 21:41:22 +0800

    完成作业第一题

---
+++ src/main/java/org/six/practice/Company.java
@@ -0,0 +1,38 @@
+package org.six.practice;
+
+/**
+ * Created by yanglu on 3/20/17.
+ */
+public class Company {
+    private int companyAge;
+    private String companyName;
+    private String companyAddress;
+
+    }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyAddress() {
+        return companyAddress;
+    }
+
+    public void setAge(int companyAge){
+        this.companyAge = companyAge;
+    }
+
+    public int getCompanyAge(){
+        return companyAge;
+    }
+
+
+    public void setCompanyAddress(String companyAddress) {
+        this.companyAddress = companyAddress;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+}
+
