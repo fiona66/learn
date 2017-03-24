fe529f201bfa6a44a9ddbd8c416b68622a32ee2a
fiona66 <lulu_fiona@163.com>
2017-03-24 09:59:38 +0800

    完成作业第二题

--- src/main/java/org/six/practice/Company.java
+++ src/main/java/org/six/practice/Company.java
@@ -8,12 +8,12 @@ public class Company {
     private String companyName;
     private String companyAddress;
 
-    public Company(String companyName,int companyAge,String companyAddress){
+    public Company(String companyName, int companyAge, String companyAddress) {
         this.companyName = companyName;
         this.companyAge = companyAge;
         this.companyAddress = companyAddress;
+//        System.out.println("公司名称:"+companyName+"\n"+"公司成立年限:"+companyAge+"\n"+"公司地址是:"+companyAddress);
     }
-
 
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
@@ -31,14 +31,31 @@ public class Company {
         return companyAddress;
     }
 
-    public void setAge(int companyAge){
+    public void setAge(int companyAge) {
         this.companyAge = companyAge;
     }
 
-    public int getCompanyAge(){
+    public int getCompanyAge() {
         return companyAge;
     }
 
+    @Override
+    public String toString() {
+        return "Company{" +
+                "companyAge=" + companyAge +
+                ", companyName='" + companyName + '\'' +
+                ", companyAddress='" + companyAddress + '\'' +
+                '}';
+    }
+
+
+    public static void main(String[] args) {
+        Company yiguo = new Company("易果生鲜", 11, "上海长宁区金钟路999号C座易贸大厦8楼");
+        System.out.println(yiguo);
+    }
 
 }
+
+
+
 
