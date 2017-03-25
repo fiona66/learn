fafa4ef5e1c5daf41e5bd77f153318fa5dcb646c
fiona66 <lulu_fiona@163.com>
2017-03-26 00:37:17 +0800

    water teach six some demo code

--- src/test/java/org/six/practice/CompanyTest.java
+++ src/test/java/org/six/practice/CompanyTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -33,7 +35,7 @@ public class CompanyTest {
     }
 
     @Test
-    public void testCompanySentOffer(){
+    public void testyiguoSentOffer(){
         // 实例化一个名叫yiguo的对象并赋值
         Company yiguo = new Company("哔哩哔哩", 11, "上海长宁区金钟路999号C座易贸大厦8楼");
         // 赋值,将该对象调用hasSentOfferToSix这个方法的值给offer
@@ -43,4 +45,37 @@ public class CompanyTest {
 
     }
 
+    @Test
+    public void testSentOfferToSix(){
+        //demo code. 涉及高级特性 泛型.
+        ArrayList<Company> companies = new ArrayList<>();  //创建一个list.
+        Company company = new Company("test", 1, "test address");
+        companies.add(company);  // 塞进去一个元素.
+
+        //遍历一个list.
+        for (int i = 0; i < companies.size(); i++) {
+            // do something.
+            System.out.println(companies.get(i));
+
+        }
+
+        //遍历list  方式二
+        for (Company comp : companies) {
+            // do something.
+            System.out.println(comp);
+        }
+
+
+
+
+
+        // 创建5个company对象放入list<company>中
+
+        // 循环遍历list中的5个公司,调用hasSentOfferToSix方法
+        // 若给了offer,打印“(该公司)给六六发了Offer”,计数器+1
+        // 若未给offer,则打印“(该公司)没有给六六发Offer”
+
+
+        // 断言,计数器的值是否为4
+    }
 }
