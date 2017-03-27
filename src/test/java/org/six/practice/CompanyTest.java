92810991dc7804bfbef07d63908fede4802ef81a
fiona66 <lulu_fiona@163.com>
2017-03-27 11:37:10 +0800

    finish homework NO.4

--- src/test/java/org/six/practice/CompanyTest.java
+++ src/test/java/org/six/practice/CompanyTest.java
@@ -7,8 +7,6 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by yanglu on 3/24/17.
  */
@@ -47,35 +45,35 @@ public class CompanyTest {
 
     @Test
     public void testSentOfferToSix(){
-        //demo code. 涉及高级特性 泛型.
-        ArrayList<Company> companies = new ArrayList<>();  //创建一个list.
-        Company company = new Company("test", 1, "test address");
-        companies.add(company);  // 塞进去一个元素.
-
-        //遍历一个list.
-        for (int i = 0; i < companies.size(); i++) {
-            // do something.
-            System.out.println(companies.get(i));
-
-        }
-
-        //遍历list  方式二
-        for (Company comp : companies) {
-            // do something.
-            System.out.println(comp);
-        }
-
-
-
-
-
         // 创建5个company对象放入list<company>中
+        ArrayList<Company> companies = new ArrayList<>();
+        Company yiguo = new Company("易果生鲜",11, "上海长宁区金钟路999号C座易贸大厦");
+        Company liulishuo = new Company("英语流利说",5,"上海杨浦区大连路970号海上海");
+        Company bilibili = new Company("哔哩哔哩",8,"上海市浦东新区浦东大道720号国际航运金融大厦 ");
+        Company teambition = new Company("Teambition",6,"上海浦东新区上海市浦东新区畅星大厦");
+        Company ctrip = new Company("携程",18,"上海长宁区金钟路968号凌空SOHO");
+
+        companies.add(yiguo);
+        companies.add(liulishuo);
+        companies.add(bilibili);
+        companies.add(teambition);
+        companies.add(ctrip);
 
         // 循环遍历list中的5个公司,调用hasSentOfferToSix方法
         // 若给了offer,打印“(该公司)给六六发了Offer”,计数器+1
         // 若未给offer,则打印“(该公司)没有给六六发Offer”
-
+        int counter = 0;
+        for (int i = 0; i < companies.size(); i++) {
+            if(companies.get(i).hasSentOfferToSix()){
+                System.out.println(companies.get(i).getCompanyName()+"给六六发了offer");
+                counter = counter + 1;
+            }else{
+                System.out.println(companies.get(i).getCompanyName()+"没有给六六发offer");
+            }
+        }
 
         // 断言,计数器的值是否为4
+        Assert.assertEquals(4,counter);
+
     }
 }
