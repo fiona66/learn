e2b5d9176024abe5d8595deb1b869ccf8cb03a12
fiona66 <lulu_fiona@163.com>
2017-03-28 10:37:29 +0800

    redo homework No.1

---
+++ src/main/java/org/six/Company.java
@@ -0,0 +1,35 @@
+package org.six;
+
+/**
+ * Created by yanglu on 3/28/17.
+ */
+public class Company {
+    private String companyName;
+    private int companyAge;
+    private String companyAddtree;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getCompanyAge() {
+        return companyAge;
+    }
+
+    public void setCompanyAge(int companyAge) {
+        this.companyAge = companyAge;
+    }
+
+    public String getCompanyAddtree() {
+        return companyAddtree;
+    }
+
+    public void setCompanyAddtree(String companyAddtree) {
+        this.companyAddtree = companyAddtree;
+    }
+}
+
