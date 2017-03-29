46351986c2a1a24539dff87bcaa61e385d0f745e
fiona66 <lulu_fiona@163.com>
2017-03-29 23:00:43 +0800

    提交作业第一题

--- src/main/java/org/six/Company.java
+++ src/main/java/org/six/Company.java
@@ -6,30 +6,36 @@ package org.six;
 public class Company {
     private String companyName;
     private int companyAge;
-    private String companyAddtree;
+    private String companyAddress;
 
+    //添加companyName属性的get类
     public String getCompanyName() {
         return companyName;
     }
 
+    //添加companyName属性的set类
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
 
+    //添加companyAge属性的get类
     public int getCompanyAge() {
         return companyAge;
     }
 
-    public void setCompanyAge(int companyAge) {
+    //添加companyAge属性的set类
+    public void companyAddress(int companyAge) {
         this.companyAge = companyAge;
     }
 
-    public String getCompanyAddtree() {
-        return companyAddtree;
+    //添加companyAddress属性的get类
+    public String getCompanyAddress() {
+        return companyAddress;
     }
 
-    public void setCompanyAddtree(String companyAddtree) {
-        this.companyAddtree = companyAddtree;
+    //添加companyName属性的get类
+    public void setCompanyAge(String companyAddress) {
+        this.companyAddress = companyAddress;
     }
 }
 
