b3692db86e458993ced8c82a037b8927b7ec043c
fiona66 <lulu_fiona@163.com>
2017-04-07 10:27:02 +0800

    method 2 to solve homework No.1

--- src/main/java/org/six/PrimeNumber.java
+++ src/main/java/org/six/PrimeNumber.java
@@ -1,25 +1,31 @@
 package org.six;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 /**
  * Created by yanglu on 4/7/17.
  */
 public class PrimeNumber {
-    // 写法1
-    public static void main(String[] args) {
-        int count = 0;
-        for(int i=101;i<=200;i++){
-            boolean b = true;
-            for(int x=2;x<i;x++){
-                if(i%x==0){
-                    b = false;
-                    break;
-                }
+    // 写法2
+    // 注意点,主函数中须调用该方法,该方法需要修饰为static
+    private static boolean isPrimeNumber(int i){
+        for(int x=2;x<i;x++){
+            if(i%x==0){
+                return false;
             }
-            if(b==true){
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int count=0;
+        for(int i=101;i<=200;i++){
+            boolean b = isPrimeNumber(i);
+            if (b==true){
                 count++;
                 System.out.println(i+"是素数");
             }
         }
-        System.out.println("101-200之间素数一共有:"+count+"个");
+        System.out.println("101-200之内的素数有"+count+"个");
     }
 }
