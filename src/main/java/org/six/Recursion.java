15870d0ef4190f79d8f0f6c77719f21480b433db
fiona66 <lulu_fiona@163.com>
2017-04-11 18:26:31 +0800

    finish recursionTest.java

--- src/main/java/org/six/Recursion.java
+++ src/main/java/org/six/Recursion.java
@@ -4,16 +4,12 @@ package org.six;
  * Created by yanglu on 4/10/17.
  */
 public class Recursion {
-    private static int recursion(int n) {
+    public static int recursion(int n) {
         if (n == 1) {
             return 1;
         } else {
             int result = n * recursion(n - 1);
             return result;
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println(recursion(5));
     }
 }
