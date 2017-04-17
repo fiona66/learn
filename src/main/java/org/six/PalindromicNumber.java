5adbde29a439ff927688cdd887318a531fee3843
fiona66 <lulu_fiona@163.com>
2017-04-17 17:39:16 +0800

    optimized code of Homework No.11

--- src/main/java/org/six/PalindromicNumber.java
+++ src/main/java/org/six/PalindromicNumber.java
@@ -5,23 +5,23 @@ package org.six;
  */
 // 回文数
 public class PalindromicNumber {
-    static int count =0;
     private static boolean isPlindramicNumber(int i){
         int unit = i % 10;
         int tens = (i / 10) % 10;
         int kbit = (i/1000)%10;
         int myriabit = i/10000;
         if (unit==myriabit && tens==kbit) {
-            count++;
             return true;
         }
         return false;
     }
 
     public static void main(String[] args) {
+        int count =0;
         for (int i = 10000; i <= 99999; i++) {
-            if(isPlindramicNumber(i)==true){
+            if(isPlindramicNumber(i)){
                 System.out.println(i+"是回文数");
+                count++;
             }
         }
         System.out.println("五位数的回文数一共有"+count+"个");
