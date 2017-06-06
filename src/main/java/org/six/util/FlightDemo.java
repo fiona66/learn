0dd4acc3795255493f263dc3d0f389911e53461c
fiona66 <lulu_fiona@163.com>
2017-06-06 09:39:02 +0800

    add Flightminprice to Mysql

--- src/main/java/org/six/util/FlightDemo.java
+++ src/main/java/org/six/util/FlightDemo.java
@@ -12,14 +12,13 @@ import java.util.HashMap;
  * Created by yanglu on 5/7/17.
  */
 public class FlightDemo {
-    public static void main(String[] args) throws ParseException, IOException {
-//        Connection con =Jsoup.connect("http://flights.ch.com/Flights/SearchByTime");
+    public int getMinPrice() throws ParseException, IOException {
         Document document = Jsoup.connect("https://flights.ch.com/Flights/SearchByTime")
                 .data("Currency", "0")
                 .data("SType", "0")
                 .data("Departure", "沙巴")
                 .data("Arrival","上海")
-                .data("DepartureDate", "2017-06-03")
+                .data("DepartureDate", "2017-06-08")
                 .data("ReturnDate","")
                 .data("IsIJFlight", "false")
                 .data("IsBg", "false")
@@ -30,17 +29,46 @@ public class FlightDemo {
                 .data("IfRet", "false")
                 .ignoreContentType(true)
                 .userAgent("Chrome")
-
                 .post();
-        System.out.println(document.toString());
         String json = document.text();
         Gson gson=new Gson();
         HashMap res = gson.fromJson(json, HashMap.class);
         Object value1 =res.get("MinPrice");
         Double value2 = (Double) value1;
-        Integer value3 = value2.intValue();
-        System.out.println(value3);
-//        System.out.println(res.keySet());
+        Integer minprice = value2.intValue();
+        System.out.println(minprice);
+        return minprice;
+    }
 
+
+    public Object getFlightNumber() throws IOException {
+        Document document = Jsoup.connect("https://flights.ch.com/Flights/SearchByTime")
+                .data("Currency", "0")
+                .data("SType", "0")
+                .data("Departure", "沙巴")
+                .data("Arrival","上海")
+                .data("DepartureDate", "2017-06-08")
+                .data("ReturnDate","")
+                .data("IsIJFlight", "false")
+                .data("IsBg", "false")
+                .data("IsEmployee", "false")
+                .data("IsLittleGroupFlight", "false")
+                .data("SeatsNum", "1")
+                .data("ActId", "0")
+                .data("IfRet", "false")
+                .ignoreContentType(true)
+                .userAgent("Chrome")
+                .post();
+        String json = document.text();
+        Gson gson=new Gson();
+        HashMap res = gson.fromJson(json, HashMap.class);
+        Object value3 =res.get("Route");
+        System.out.println(value3);
+        String value4 = document.text();
+        HashMap res1 = gson.fromJson(value4,HashMap.class);
+        Object FlightNumber = res1.get("FlightNumber");
+        System.out.println(FlightNumber);
+
+        return FlightNumber;
     }
 }
--- src/main/java/org/six/util/JDBCDemo.java
+++ src/main/java/org/six/util/JDBCDemo.java
@@ -1,24 +1,33 @@
 package org.six.util;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+//import java.sql.*;
 import java.sql.*;
-import java.util.Properties;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by yanglu on 5/7/17.
  */
 public class JDBCDemo {
         static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        static final String DB_URL = "jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12173107";
+//        static final String DB_URL = "jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12173107";
+        static final String DB_URL="jdbc:mysql://127.0.0.1:3306/flight_min_price";
 
-        //  Database credentials
-        static final String USER = "sql12173107";
-        static final String PASS = "VundJa6yE9";
+    //  Database credentials
+        static final String USER = "root";
+        static final String PASS = "FIONAYL0510";
 
-    public static void main(String[] args) {
+
+    public void addToMySQL() throws IOException, ParseException {
         Connection conn = null;
         Statement stmt = null;
+        Date date=new Date();
+        SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd HH:mm:ss");
+//        date currentdate=date.getTime();
+        FlightDemo getminprice=new FlightDemo();
+        int minprice=getminprice.getMinPrice();
         try{
             //STEP 2: Register JDBC driver
             Class.forName("com.mysql.jdbc.Driver");
@@ -31,27 +40,30 @@ public class JDBCDemo {
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             String sql;
-            sql = "SELECT * FROM flight_price";
-//            sql = "INSERT INTO flight_table(id,flight_number,minprice) VALUES(4,'123456',333)";
-//            sql.executeUpdate("INSERT INTO Sessions(id_user) VALUES(1)"); // DML operation
-//            sql.executeQuery("SELECT LAST_INSERT_ID()"); // SELECT operation
-            int rs1 = stmt.executeUpdate("INSERT INTO flight_price(id,flight_number,minprice) VALUES(5,'123456',333)");
+            sql = "SELECT * FROM flightminprice";
+            String demo = String.valueOf(Math.random()).substring(2,5);
+            System.out.println(demo);
+            String sql2="INSERT INTO flightminprice(flight_num,minprice,querytime) VALUES ('"+ demo +"' ,'"+ minprice +"','"+ ft.format(date)+"')";
+
+//            String sql2="INSERT INTO flightminprice(flight_num,minprice,querytime) VALUES"+"('Demo' " + ",123"+",'+date.toString()')";
+//            int rs1 = stmt.executeUpdate("INSERT INTO flightminprice(flight_num,minprice,querytime) VALUES('Demo',123456,'2017-06-05 18:48:40')");
+            int rs1 = stmt.executeUpdate(sql2);
             ResultSet rs = stmt.executeQuery(sql);
-
-
-
 
             //STEP 5: Extract data from result set
             while(rs.next()){
                 //Retrieve by column name
                 int id  = rs.getInt("id");
-                String flightNumber = rs.getString("flight_number");
+                String flightNumber = rs.getString("flight_num");
                 String minPrice = rs.getString("minprice");
+                String querytime = rs.getString("querytime");
 
                 //Display values
                 System.out.print("id: " + id);
                 System.out.print(", FlightNumber: " + flightNumber);
                 System.out.print(", MinPrice: " + minPrice);
+                System.out.print(", Querytime: " + querytime);
+
             }
             //STEP 6: Clean-up environment
             rs.close();
@@ -80,37 +92,4 @@ public class JDBCDemo {
         System.out.println("Goodbye!");
     }//end main
 
-
-//        Class.forName("com.mysql.jdbc.Driver");
-//        String url = "jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12173107";
-//        Properties props = new Properties();
-//        FileInputStream propertiesFile = new FileInputStream("JDBC.properties");
-//        props.load(propertiesFile);
-//        propertiesFile.close();
-//
-//        String DriverStr = props.getProperty("jdbc.Driver");
-//        String urlStr = props.getProperty("jdbc.url");
-//        String userName = props.getProperty("jdbc.name");
-//        String passcode = props.getProperty("jdbc.passworld");
-//
-//        //打开数据库链接
-//        Connection connection = DriverManager.getConnection(urlStr,userName,passcode);
-//        //执行SQL
-//        Statement sta = connection.createStatement();
-//        //executeUpdate可以返回数据库更新的行数
-//        int efactRow = sta.executeUpdate("UPDATE USER SET Permition = 'admin' WHERE username = 'Zing'");
-//        //executeQuery可以返回一个查询的结果集，这个集合的迭代器略有不同Iterator,没有hasNext方法，初始是，指针在数据前，必须调用next方法才能读取第一行数据
-//        ResultSet resultSet = sta.executeQuery("SELECT * FROM USER ;");
-//        while (resultSet.next()){
-//            //当前行获取第一栏的值，具体类型需要看数据库实现
-//            resultSet.getString(1);
-//        }
-//        //关闭语句
-//        sta.close();
-//        //关闭结果集
-//        resultSet.close();
-//        //关闭数据库连接
-//        connection.close();
-//
-
-    }
+}
