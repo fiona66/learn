03bc025fd58730a4e0c698967eb2c2cc8a6c0985
fiona66 <lu.yang@duitang.com>
2018-06-04 19:00:45 +0800

    Revert "Exercise20180418"

---
+++ src/main/java/org/six/practice/FreshJuice.java
@@ -0,0 +1,9 @@
+package org.six.practice;
+
+/**
+ * Created by yanglu on 3/19/17.
+ */
+class FreshJuice {
+    enum FreshJuiceSize{ SMALL, MEDIUM , LARGE }
+    FreshJuiceSize size;
+}
---
+++ src/test/java/org/six/practice/FreshJuiceTest.java
@@ -0,0 +1,14 @@
+package org.six.practice;
+
+
+/**
+ * Created by yanglu on 3/19/17.
+ */
+
+
+public class FreshJuiceTest {
+    public static void main(String []args){
+        FreshJuice juice = new FreshJuice();
+        juice.size = FreshJuice. FreshJuiceSize.MEDIUM  ;
+    }
+}
